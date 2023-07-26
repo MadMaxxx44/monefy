6197191a797304104a269b8dc17e92c76d5d1284
Max <andriiuk.maks@gmail.com>
2023-07-26 09:24:04 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen w-screen justify-center items-center">
+        <section className="flex h-3/5 w-3/5">
+          <div className="flex h-10 w-full bg-green-500 justify-center items-center text-2xl">
+            <h1 className="font-bold">Monefy</h1>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default App;
